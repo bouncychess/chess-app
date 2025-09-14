3c3e332b573f98aae9061dd7396a6b25b6300f03
Christopher A <catkeson@Mac.attlocal.net>
2025-09-13 23:22:57 -0700

    add websocket context

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Play from './pages/Play/Play';
 import Layout from './components/Layout';
+import { WebSocketProvider } from './context/WebSocketContext';
 
 export default function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Layout><Home /></Layout>} />
-                <Route path="/play" element={<Layout><Play /></Layout>} />
-            </Routes>
-        </Router>
+        <WebSocketProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Layout><Home /></Layout>} />
+                    <Route path="/play" element={<Layout><Play /></Layout>} />
+                </Routes>
+            </Router>
+        </WebSocketProvider>
     );
 }
