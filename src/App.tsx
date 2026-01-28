8c0341bc2000745bb1e4deeba404c49ea19f4f5f
“Hayden <haydenkarkainen@gmail.com>
2026-01-27 21:18:29 -0800

    fixing merge issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-<<<<<<< HEAD
-import Home from './pages/Home/Home';
-=======
->>>>>>> origin/main
 import News from './pages/News/News';
 import Play from './pages/Play/Play';
 import Game from './pages/Game/Game';
@@ -21,18 +17,11 @@ export default function App() {
                 <Router>
                     <Routes>
                         <Route path="/" element={<Navigate to="/play" replace />} />
-<<<<<<< HEAD
-                        <Route path="/play" element={<ProtectedRoute><Layout><Play /></Layout></ProtectedRoute>} />
-                        <Route path="/game/:gameId" element={<ProtectedRoute><Layout><Game /></Layout></ProtectedRoute>} />
-                        <Route path="/home" element={<ProtectedRoute><Layout><Home /></Layout></ProtectedRoute>} />
-                        <Route path="/news" element={<ProtectedRoute><Layout><News /></Layout></ProtectedRoute>} />
-=======
                         <Route path="/play" element={<Layout><Play /></Layout>} />
                         <Route path="/game/:gameId" element={<Layout><Game /></Layout>} />
                         <Route path="/news" element={<Layout><News /></Layout>} />
                         <Route path="/profile" element={<Layout><Profile /></Layout>} />
                         <Route path="/signin" element={<Layout><SignIn /></Layout>} />
->>>>>>> origin/main
                         <Route path="/articles/:id" element={<Layout><Article /></Layout>} />
                         <Route path="/logout" element={<Logout />} />
                     </Routes>
