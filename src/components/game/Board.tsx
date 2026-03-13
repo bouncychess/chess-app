1c7dde043318ca4210dcc17df2b9150b6c383eb3
Christopher A <ctatkeson@gmail.com>
2026-03-12 19:44:52 -0700

    revert

--- src/components/game/Board.tsx
+++ src/components/game/Board.tsx
@@ -49,7 +49,7 @@ function Board({ gameId, playerColor, initialTurn, initialPgn, onTurnChange, onP
   const playMoveSound = () => {
     moveSoundRef.current.currentTime = 0;
     moveSoundRef.current.volume = 1;
-    moveSoundRef.current.playbackRate = 2;
+    moveSoundRef.current.playbackRate = 1;
     moveSoundRef.current.play().catch(() => {});
   };
 
