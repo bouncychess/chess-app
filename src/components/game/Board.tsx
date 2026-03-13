6b5807b1af22ab3180260456efe132525db550a8
Christopher A <ctatkeson@gmail.com>
2026-03-12 19:49:05 -0700

    lower volume

--- src/components/game/Board.tsx
+++ src/components/game/Board.tsx
@@ -48,6 +48,8 @@ function Board({ gameId, playerColor, initialTurn, initialPgn, onTurnChange, onP
 
   const playMoveSound = () => {
     moveSoundRef.current.currentTime = 0;
+    moveSoundRef.current.volume = .6;
+    moveSoundRef.current.playbackRate = 1;
     moveSoundRef.current.play().catch(() => {});
   };
 
