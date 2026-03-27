f64b752f9a16c46879305dded4ec9ca7581cdecb
Christopher A <ctatkeson@gmail.com>
2026-03-27 08:42:26 -0700

    deploy bouncychess

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -12,14 +12,6 @@ const SettingsIcon = () => (
 );
 
 // Simple SVG icons
-const NewsIcon = () => (
-    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-        <path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2zm0 0a2 2 0 0 1-2-2v-9c0-1.1.9-2 2-2h2" />
-        <path d="M18 14h-8" />
-        <path d="M15 18h-5" />
-        <path d="M10 6h8v4h-8V6z" />
-    </svg>
-);
 
 const PlayIcon = () => (
     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
@@ -139,12 +131,6 @@ function SidebarContent({ collapsed, onCollapse, onExpand, showToggle = true }: 
 
             <nav style={{ flex: 1 }}>
                 <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
-                    <li>
-                        <Link to="/news" style={linkStyle} title="News">
-                            <NewsIcon />
-                            {!collapsed && <span>News</span>}
-                        </Link>
-                    </li>
                     <li>
                         <Link to="/play" style={linkStyle} title="Play">
                             <PlayIcon />
